e29802f470741dc204da7ee17e34da8d522ac0e0
Vishal2827 <vishu28282828@gmail.com>
2025-08-24 08:32:32 +0000

    Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
                   className="font-semibold font-mono tracking-widest text-2xl 
                     bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
                 >
-                  VISHAL'S POSGRESTORE
+                  VISHAL'S POSGRES
                 </span>
               </div>
             </Link>
